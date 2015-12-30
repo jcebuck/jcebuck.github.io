1bdf8b2db3aa541d3459b4a487447c196ed28342
James Buck <jcebuck@gmail.com>
2015-12-30 23:15:28 +0000

    Fixed active links on navbar

--- js/default.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	var addr = ['gmail.com', 'jcebuck'];
-	addr.reverse();
-
-	$('li[data-url="{{ page.url }}"]').addClass('active');
-	$('.social-buttons').find('.btn-google').attr('href', 'mailto:' + addr.join('@'));
-});
